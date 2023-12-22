3dbd04e5730f1e2a9dd2f33f18b5ce48b38604ad
dailyminiii <79134282+dailyminiii@users.noreply.github.com>
2023-12-22 18:40:25 +0900

    Updates

--- 01_data_preprocessing_merge.py
+++ 01_data_preprocessing_merge.py
@@ -1,11 +1,35 @@
+############
+# MIT License
+#
+# Copyright (c) 2023 Minwoo Seong and 2022 MIT CSAIL and Joseph DelPreto
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# This file is a modified version originally written by Joseph DelPreto.
+# Source of the original version: https://github.com/delpreto/ActionNet/blob/master/parsing_data/example_activity_classification/01_create_examples.py
+# Date of modification: 2023.12.22
+# Modified by: Minwoo Seong
+# See https://action-net.csail.mit.edu for more usage information.
+
 import h5py
-import numpy as np
 from scipy import interpolate  # for resampling
 from scipy.signal import butter, lfilter  # for filtering
-import pandas as pd
-from matplotlib import pyplot as plt
-import pickle
-from collections import OrderedDict
 import os, glob
 import pandas as pd
 
@@ -14,7 +38,6 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 # from helpers import *
 from utils.print_utils import *
 from utils.dict_utils import *
-from utils.time_utils import *
 
 #######################################
 ############ CONFIGURATION ############
--- utils/dict_utils.py
+++ utils/dict_utils.py
@@ -1,6 +1,7 @@
 ############
+# MIT License
 #
-# Copyright (c) 2022 MIT CSAIL and Joseph DelPreto
+# Copyright (c) 2023 Minwoo Seong and 2022 MIT CSAIL and Joseph DelPreto
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -15,14 +16,16 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-# IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 #
+# This file is a modified version originally written by Joseph DelPreto.
+# Source of the original version: https://github.com/delpreto/ActionNet/blob/master/parsing_data/example_activity_classification/01_create_examples.py
+# Date of modification: 2023.12.22
+# Modified by: Minwoo Seong
 # See https://action-net.csail.mit.edu for more usage information.
-# Created 2021-2022 for the MIT ActionNet project by Joseph DelPreto [https://josephdelpreto.com].
-#
-############
 
 from collections import OrderedDict
 
